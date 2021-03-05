5f019c00473bdf88e26af0dc6e5d2f2606970f48
Chad Cook <Charles.Cook28@gmail.com>
2021-03-04 23:13:40 -0600

    added some more styling

--- public/index.js
+++ public/index.js
@@ -71,7 +71,7 @@ function populateChart() {
         datasets: [{
             label: "Total Over Time",
             fill: true,
-            backgroundColor: "#6666ff",
+            backgroundColor: "rgba(37, 68, 247,.75)",
             data
         }]
     }
